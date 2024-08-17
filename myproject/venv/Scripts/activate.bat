d4fa02e3595d01e47526ed069119dd9dc04061ed
Alejandrapulido85 <mpulido85@uan.edu.co>
2024-08-17 18:38:25 -0500

    prueba base datos

--- myproject/venv/Scripts/activate.bat
+++ myproject/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\57313\prueba_trabajo_grado\myproject\venv
+set VIRTUAL_ENV=C:\Users\aleja\Documents\PROYECTO GRADO\prueba_trabajo_grado\myproject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
